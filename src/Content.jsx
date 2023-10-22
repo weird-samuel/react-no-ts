c78c2c8fb7a708f5f96d61b50115122f875501cc
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-10-22 22:49:32 +0100

    understood state

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 const Content = () => {
   const [name, setName] = useState("Samuel");
+  const [count, setCount] = useState(0);
 
-  const HandleNameChange = () => {
+  const handleNameChange = () => {
     const names = ["John", "Jane", "Joe"];
     const idx = Math.floor(Math.random() * names.length);
     return setName(names[idx]);
   };
 
   const handleClick2 = () => {
-    console.log("You clicked me!");
+    setCount(count + 1);
+    console.log(count);
   };
 
   return (
     <main>
       <p>Hello {name} !</p>
-      <button onClick={HandleNameChange}>Click me</button>
+      <button onClick={handleNameChange}>Click me</button>
+      <button onClick={handleClick2}>Count</button>
       <button onClick={() => handleClick2("Samuel")}>Button 2</button>
     </main>
   );

a78527dc7d78a9ad75fa0355fe0ae56dc3587919
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-10-28 10:32:21 +0100

    todo-app-react

---
+++ src/NewTodoForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export function NewTodoForm({ onSubmit }) {
+  onSubmit;
+  const [newIetm, setNewItem] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (newIetm === "") return;
+
+    onSubmit(newIetm);
+    setNewItem("");
+  }
+  return (
+    <form className="new-item-form" onSubmit={handleSubmit}>
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input
+          value={newIetm}
+          onChange={(e) => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
+}
